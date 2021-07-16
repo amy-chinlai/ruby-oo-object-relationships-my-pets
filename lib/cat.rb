655e812e4ced04b2ca4d0f7ecb89ccf9c50f13d6
amy-chinlai <amy@actionnetwork.org>
2021-07-16 16:12:09 -0400

    commit tree 64d469f6487bb892487726511b458e28bb90221c

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,24 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+
 end
